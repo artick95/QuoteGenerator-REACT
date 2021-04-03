1c978dc2281579349be29be425d4b613ea91ceb9
artick95 <@Kalel0964>
2021-04-03 17:07:25 +0200

    more ore less styled

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import React, { useState } from "react";
 
 function App() {
+  //function api request for the quote
+  // https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?
+  const [quote, setQuote] = useState(
+    "Those who excel in virtue have the best right of all to rebel, but then they are of all men the least inclined to do so."
+  );
+
+  async function getQuote() {
+    const apiUrl = "https://goquotes-api.herokuapp.com/api/v1/random?count=1";
+    axios
+      .get(apiUrl)
+      .then((response) => setQuote(response.data.quotes[0].text))
+      .catch((error) => console.log(error));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="card">
+        <div className="textButtonContainer">
+          <h2 className="text">"{quote}"</h2>
+          <button type="button" className="button" onClick={() => getQuote()}>
+            Generate New Quote
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
